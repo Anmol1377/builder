b118fcc43efff606ff24a5060849533a693a155a
Anmol <anmolkumar58575@gmail.com>
2022-05-15 13:22:24 +0530

    ok

---
+++ build/precache-manifest.81c2d7aef924cd3e49a6dc18400d8938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896bb81164ad45b92d39535ce7d8d873",
+    "url": "/builder/index.html"
+  },
+  {
+    "revision": "9c7d2a21e752aec64499",
+    "url": "/builder/static/css/main.db7ec9a3.chunk.css"
+  },
+  {
+    "revision": "aa107786ee90dc895370",
+    "url": "/builder/static/js/2.4339f294.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/builder/static/js/2.4339f294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7d2a21e752aec64499",
+    "url": "/builder/static/js/main.7aa178f1.chunk.js"
+  },
+  {
+    "revision": "ec6be0b9a800aff216eb",
+    "url": "/builder/static/js/runtime-main.2ca82786.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e89c798e2c1c9ca9e992a217380a6b1.js"
+  "/builder/precache-manifest.81c2d7aef924cd3e49a6dc18400d8938.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
